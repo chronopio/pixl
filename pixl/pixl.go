14f62a2fd57d8cc50891a30e8ef442a3b72124cf
chronopio <joaquinalejandro00@gmail.com>
2024-06-16 23:56:22 +0200

    Add main module

---
+++ pixl/pixl.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"image/color"
+	"pixl/apptype"
+	"pixl/pxcanvas"
+	"pixl/swatch"
+	"pixl/ui"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+)
+
+func main() {
+	pixlApp := app.New()
+	pixlWindow := pixlApp.NewWindow("Pixl")
+
+	state := apptype.State{
+		BrushColor:     color.NRGBA{255, 255, 255, 255},
+		SwatchSelected: 0,
+	}
+
+	pixlCanvasConfig := apptype.PxCanvasConfig{
+		DrawingAreaSize: fyne.NewSize(600, 600),
+		CanvasOffset:    fyne.NewPos(0, 0),
+		PxRows:          20,
+		PxCols:          20,
+		PxSize:          30,
+	}
+
+	pixlCanvas := pxcanvas.NewPxCanvas(&state, pixlCanvasConfig)
+
+	appInit := ui.AppInit{
+		PixlCanvas: pixlCanvas,
+		PixlWindow: pixlWindow,
+		State:      &state,
+		Swatches:   make([]*swatch.Swatch, 0, 64),
+	}
+
+	ui.Setup(&appInit)
+
+	appInit.PixlWindow.ShowAndRun()
+}
